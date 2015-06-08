d840d57b7318240f13b6965defbe0721259b2078
Vijeta Patel <vijetaptl@gmail.com>
2015-06-08 11:22:56 -0700

    Initial WebDriverTester check-in
    Includes the main test page to send Http requests to a browser. There is
    a small set of commands that are pre-loaded, with more to be added
    later. Includes a small FAQ.

---
+++ src/js/WebDriverTester.js
@@ -0,0 +1,219 @@
+﻿var lastCommandSent;
+
+function setupCommands() {
+    // This list is in alphabetical order by commandName
+    loadCommands();
+    var s = document.getElementById("commands-select");
+    for (var j = 0; j < commands.length; j++) {
+        var o = document.createElement("OPTION");
+        o.value = commands[j].commandName;
+        o.innerHTML = commands[j].commandName;
+
+        s.appendChild(o);
+    }
+}
+
+function setupHttpMethods() {
+    var methods = ["GET", "POST", "DELETE"];
+
+    var s = document.getElementById("methods-select");
+    for (var j = 0; j < methods.length; j++) {
+        var o = document.createElement("OPTION");
+        o.value = methods[j];
+        o.innerHTML = methods[j];
+        s.appendChild(o);
+        o.selected = true;
+    }
+}
+
+function setup()
+{
+	setupCommands();
+	setupHttpMethods();
+}
+
+function checkForIds(str) {
+    var s = document.getElementById("session-select");
+    var index = -1;
+    if (s.size == 1) {
+        index = 0;
+    }
+    else {
+        index = s.selectedIndex;
+    }
+
+    if (index >= 0) {
+        var sessionId = document.getElementById("session-select").childNodes[index].value;
+        str = str.replace("SESSION_ID", sessionId);
+    }
+
+    return str;
+}
+
+function updateCommand() {
+    var s = document.getElementById("commands-select");
+
+    for (var i = 0; i < commands.length; i++) {
+        if (commands[i].commandName == s.options[s.selectedIndex].value) {
+            document.getElementById("methods-select").value = commands[i].method;
+            document.getElementById("path").value = checkForIds(commands[i].path);
+
+            if (commands[i].requestBody != "") {
+                try {
+                    var jsonObj = JSON.parse(commands[i].requestBody);
+                    var jsonString = JSON.stringify(jsonObj, null, 4);
+                    document.getElementById("content-area").value = checkForIds(jsonString);
+                }
+                catch (err) {
+                    logError(err.message);
+                }
+            }
+            else {
+                document.getElementById("content-area").value = "";
+            }
+            break;
+        }
+    }
+}
+
+function formatJSON() {
+    try {
+        var t = document.getElementById("content-area");
+        var jsonObj = JSON.parse(t.value);
+        var jsonString = JSON.stringify(jsonObj, null, 4);
+        t.value = jsonString;
+    }
+    catch (err) {
+        logError(err.message);
+    }
+}
+
+function logRequest(method, url, requestBody) {
+    var lRequest = "<div class=\"bg-info\">" + "<p>" + getTimeString() + "- Request " + method + " " + url + "</p>";
+    if (requestBody != "")
+    {
+        try
+        {
+            var jsonObj = JSON.parse(requestBody);
+            var jsonString = JSON.stringify(jsonObj, null, 4);
+            lRequest += "<pre>" + jsonString + "</pre>";
+        }
+        catch (err)
+        {
+            logError(err.message);
+        }
+    }
+    lRequest += "</div>";
+
+    var d = document.createElement("div");
+    d.class = "loq-request";
+    d.innerHTML = lRequest;
+
+    var l = document.getElementById("log-contents");
+    l.insertBefore(d, l.firstChild);
+}
+
+function logResponse(status, contentBody) {
+    var lResponse = "<div class=\"bg-success\">" + "<p>" + getTimeString() + "- Response " + status + "</p>";
+    if (contentBody != "") {
+        try {
+            var jsonObj = JSON.parse(contentBody);
+            var jsonString = JSON.stringify(jsonObj, null, 4);
+            lResponse += "<pre>" + jsonString + "</pre>";
+        }
+        catch (err) {
+            // The content is not JSON
+            lResponse += "<pre>" + contentBody + "</pre>";
+        }
+    }
+    lResponse += "</div>";
+
+    var d = document.createElement("div");
+    d.class = "loq-response";
+    d.innerHTML = lResponse;
+
+    var l = document.getElementById("log-contents");
+    l.insertBefore(d, l.firstChild);
+}
+
+function logError(errMsg) {
+    var lError = "<div class=\"bg-danger\">" + "<p>" + getTimeString() + "-" + errMsg + "</p></div>";
+    var d = document.createElement("div");
+    d.class = "log-error";
+    d.innerHTML = lError;
+
+    var l = document.getElementById("log-contents");
+    l.insertBefore(d, l.firstChild);
+}
+
+function clearLog() {
+    document.getElementById("log-contents").innerHTML = "";
+}
+
+function addSessionId(sessionId) {
+    var o = document.createElement("option");
+    o.value = sessionId;
+    o.innerHTML = sessionId;
+
+    var s = document.getElementById("session-select");
+    s.appendChild(o);
+    s.size = s.length;
+}
+
+function processResponse(xmlhttp) {
+    logResponse(xmlhttp.status, xmlhttp.responseText);
+
+    if (xmlhttp.status == 200)
+    {
+        try
+        {
+            var jsonObj = JSON.parse(xmlhttp.responseText);
+            if (lastCommandSent == "newSession")
+            {
+                var sessionId = jsonObj.sessionId;
+                if (sessionId != "")
+                {
+                    addSessionId(sessionId);
+                }
+            }
+            lastCommandSent = "";
+        }
+        catch (err)
+        {
+            logError(err.message);
+        }
+        
+    }
+}
+
+function sendRequest() {
+    var portNumber = document.getElementById("port").value;
+    if (portNumber > 1024 && portNumber <= 65531) {
+        var path = document.getElementById("path").value;
+        var url = "http://localhost:" + portNumber + path;
+        var requestBody = document.getElementById("content-area").value;
+        var method = document.getElementById("methods-select").value;
+        logRequest(method, url, requestBody);
+
+        lastCommandSent = document.getElementById("commands-select").value;
+
+        var xmlhttp = new XMLHttpRequest();
+
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4) {
+                processResponse(xmlhttp);
+            }
+        }
+
+        xmlhttp.open(method, url, true);
+        if (requestBody == "") {
+            xmlhttp.send();
+        }
+        else {
+            xmlhttp.send(requestBody);
+        }
+    }
+    else {
+        logError("Port " + portNumber + " is not valid. Try another port number.")
+    }
+}
---
+++ src/js/common.js
@@ -0,0 +1,26 @@
+﻿
+var commands;
+
+function loadCommands()
+{
+    commands = [];
+    commands.push({ commandName: "executeScript", method: "POST", path: "/session/SESSION_ID/execute", requestBody: "{\"script\": \"return arguments[0].second;\",\"args\": [{\"first\":\"1st\", \"second\":\"2nd\", \"third\":\"3rd\"}]}" });
+    commands.push({ commandName: "getCapabilities", method: "GET", path: "/session/SESSION_ID", requestBody: "" });
+    commands.push({ commandName: "getCurrentUrl", method: "GET", path: "/session/SESSION_ID/url", requestBody: "" });
+    commands.push({ commandName: "getTitle", method: "GET", path: "/session/SESSION_ID/title", requestBody: "" });
+    commands.push({ commandName: "sessions", method: "GET", path: "/sessions", requestBody: "" });    commands.push({ commandName: "newSession", method: "POST", path: "/session", requestBody: "{\"name\":\"newSession\",\"sessionId\":\"\",\"parameters\":{\"desiredCapabilities\":{ \"browserName\":\"<browserName>\", \"browserVersion\":\"<browserVersion>\", \"platformName\":\"Windows NT\", \"platformVersion\":\"10\"},\"requiredCapabilities\":{ }}}" });
+    commands.push({ commandName: "status", method: "GET", path: "/status", requestBody: "" });
+    commands.push({ commandName: "quit", method: "DELETE", path: "/session/SESSION_ID", requestBody: "" });
+}
+
+function getTimeString() {
+    var currentdate = new Date();
+    var datetime = currentdate.getDate() + "/"
+				+ (currentdate.getMonth() + 1) + "/"
+				+ currentdate.getFullYear() + " "
+				+ currentdate.getHours() + ":"
+				+ currentdate.getMinutes() + ":"
+				+ currentdate.getSeconds();
+
+    return datetime;
+}
